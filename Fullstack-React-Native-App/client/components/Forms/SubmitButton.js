f06baba51ae9676ad4994166703609f09adc7116
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-07-05 19:28:06 +0530

    Completed Register UI for FSA

---
+++ Fullstack-React-Native-App/client/components/Forms/SubmitButton.js
@@ -0,0 +1,31 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+
+const SubmitButton = ({ handleSubmit, btnTitle, loading }) => {
+  return (
+    <TouchableOpacity style={styles.submitBtn} onPress={handleSubmit}>
+      <Text style={styles.submitBtnText}>
+        {loading ? "Please wait" : btnTitle}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  submitBtn: {
+    backgroundColor: "#1e2225",
+    height: 50,
+    marginHorizontal: 25,
+    borderRadius: 80,
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  submitBtnText: {
+    color: "#ffffff",
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "400",
+  },
+});
+
+export default SubmitButton;
--- Fullstack-React-Native-App/client/screens/auth/Register.js
+++ Fullstack-React-Native-App/client/screens/auth/Register.js
@@ -1,12 +1,79 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, TextInput, Alert } from "react-native";
+import React, { useState } from "react";
+import InputBox from "../../components/Forms/InputBox";
+import SubmitButton from "../../components/Forms/SubmitButton";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {
+    try {
+      setLoading(true);
+      if (!name || email || password) {
+        setLoading(false);
+        Alert.alert("Please Fill All Fields");
+        return;
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Register here</Text>
+      <View style={{ marginHorizontal: 20 }}>
+        <InputBox
+          title={"Name"}
+          autoComplete=""
+          keyboardType=""
+          value={name}
+          setValue={setName}
+          secureTextEntry=""
+        />
+        <InputBox
+          title={"Email"}
+          autoComplete="email"
+          secureTextEntry=""
+          value={email}
+          setValue={setEmail}
+          keyboardType="email-address"
+        />
+        <InputBox
+          title={"Password"}
+          autoComplete=""
+          value={password}
+          setValue={setPassword}
+          keyboardType=""
+          secureTextEntry={true}
+        />
+      </View>
+      <SubmitButton
+        btnTitle="Register"
+        loading={loading}
+        handleSubmit={handleSubmit}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#e1d5c9",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#1e2225",
+    marginBottom: 20,
+  },
+});
 
 export default Register;
