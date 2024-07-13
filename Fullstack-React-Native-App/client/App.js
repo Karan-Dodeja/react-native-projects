1d7db9f400bfb40e0738f1572e18192e2dd7722a
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-07-13 08:07:57 +0530

    Completed Log out functionality

--- Fullstack-React-Native-App/client/App.js
+++ Fullstack-React-Native-App/client/App.js
@@ -1,10 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import RootNavigation from "./navigation";
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <RootNavigation />
--- Fullstack-React-Native-App/client/components/Menus/ScreenMenus.js
+++ Fullstack-React-Native-App/client/components/Menus/ScreenMenus.js
@@ -1,21 +1,42 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../../screens/Home";
 import Register from "../../screens/auth/Register";
 import Login from "../../screens/auth/Login";
+import { AuthContext } from "../../context/authContext";
+import HeaderMenus from "./HeaderMenus";
 
 const ScreenMenus = () => {
+  // global state
+  const [state] = useContext(AuthContext);
+  // Auth condition true false
+  const authenticatedUser = state?.user && state?.token;
   const Stack = createNativeStackNavigator();
+
   return (
     <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{ headerShown: false }}
-      />
+      {authenticatedUser ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: "Full Stack App",
+              headerRight: () => <HeaderMenus />,
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
